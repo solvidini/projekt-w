bf4bfa4c947aafb8798266e99d338682c681b404
Samuel Kedziora <samukedo@gmail.com>
2020-07-26 17:42:14 +0200

    improved navigation, grid added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,75 +8,106 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import { LanguageContext } from '../../../context';
 
 const NavigationItems = (props) => {
-	const context = useContext(LanguageContext);
-	return (
-		<ul className="navigation-item-list">
-			<li className="navigation-item" onClick={props.clicked}>
-				<NavigationItem link="/">{context.dictionary.nav.home}</NavigationItem>
-			</li>
-			<li className="navigation-item" onClick={props.clicked}>
-				<NavigationItem link="/studio">{context.dictionary.nav.studio}</NavigationItem>
-			</li>
-			<li className="navigation-dropdown">
-				<span>{context.dictionary.nav.projects}&nbsp;</span>
-				<FontAwesomeIcon icon={faCaretDown} />
-				<div className="navigation-dropdown__content">
-					<NavigationItem link="/projects">{context.dictionary.nav.projects}</NavigationItem>
-					<NavigationItem link="/projects-premium">{context.dictionary.nav.projectsPremium}</NavigationItem>
-				</div>
-			</li>
-			<li className="navigation-dropdown">
-				<span>{context.dictionary.nav.realisations}&nbsp;</span>
-				<FontAwesomeIcon icon={faCaretDown} />
-				<div className="navigation-dropdown__content">
-					<NavigationItem exact link="/realisations">
-						{context.dictionary.nav.realisations}
-					</NavigationItem>
-					<NavigationItem exact link="/realisations-premium">
-						{context.dictionary.nav.realisationsPremium}
-					</NavigationItem>
-				</div>
-			</li>
-			<li className="navigation-item" onClick={props.clicked}>
-				<NavigationItem link="/offer">{context.dictionary.nav.offer}</NavigationItem>
-			</li>
-			<li className="navigation-item" onClick={props.clicked}>
-				<NavigationItem link="/partners">{context.dictionary.nav.partners}</NavigationItem>
-			</li>
-			<li className="navigation-item" onClick={props.clicked}>
-				<NavigationItem link="/contact">{context.dictionary.nav.contact}</NavigationItem>
-			</li>
-			<li className="navigation-item mobile-only" onClick={props.clicked}>
-				<NavigationItem link="/projects">{context.dictionary.nav.projects}</NavigationItem>
-			</li>
-			<li className="navigation-item mobile-only" onClick={props.clicked}>
-				<NavigationItem link="/projects-premium">&nbsp;&rarr;&nbsp;Premium</NavigationItem>
-			</li>
-			<li className="navigation-item mobile-only" onClick={props.clicked}>
-				<NavigationItem exact link="/realisations">
-					{context.dictionary.nav.realisations}
-				</NavigationItem>
-			</li>
-			<li className="navigation-item mobile-only" onClick={props.clicked}>
-				<NavigationItem exact link="/realisations-premium">
-					&nbsp;&rarr;&nbsp;Premium
-				</NavigationItem>
-			</li>
-			<li className="navigation-item" onClick={props.clicked}>
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					className="navigation-icon"
-					href="https://www.facebook.com/ProjektWPAW/"
-				>
-					<FontAwesomeIcon icon={faFacebookF} />
-					<span className="mobile-only navigation-icon__text" style={{ textTransform: 'lowercase' }}>
-						acebook
-					</span>
-				</a>
-			</li>
-		</ul>
-	);
+  const context = useContext(LanguageContext);
+  return (
+    <ul className="navigation-list">
+      <li className="navigation-item" onClick={props.clicked}>
+        <NavigationItem link="/">
+          {context.dictionary.nav.home}
+        </NavigationItem>
+      </li>
+      <li className="navigation-item" onClick={props.clicked}>
+        <NavigationItem link="/studio">
+          {context.dictionary.nav.studio}
+        </NavigationItem>
+      </li>
+      <li className="navigation-item navigation-item--dropdown">
+        <span>
+          {context.dictionary.nav.projects}&nbsp;
+          <FontAwesomeIcon icon={faCaretDown} />
+        </span>
+        <div className="navigation-item navigation-item--dropdown__content">
+          <NavigationItem link="/projects">
+            {context.dictionary.nav.projects}
+          </NavigationItem>
+          <NavigationItem link="/projects-premium">
+            {context.dictionary.nav.projectsPremium}
+          </NavigationItem>
+        </div>
+      </li>
+      <li className="navigation-item navigation-item--dropdown">
+        <span>
+          {context.dictionary.nav.realisations}&nbsp;
+          <FontAwesomeIcon icon={faCaretDown} />
+        </span>
+        <div className="navigation-item navigation-item--dropdown__content">
+          <NavigationItem exact link="/realisations">
+            {context.dictionary.nav.realisations}
+          </NavigationItem>
+          <NavigationItem exact link="/realisations-premium">
+            {context.dictionary.nav.realisationsPremium}
+          </NavigationItem>
+        </div>
+      </li>
+      <li className="navigation-item" onClick={props.clicked}>
+        <NavigationItem link="/offer">
+          {context.dictionary.nav.offer}
+        </NavigationItem>
+      </li>
+      <li className="navigation-item" onClick={props.clicked}>
+        <NavigationItem link="/partners">
+          {context.dictionary.nav.partners}
+        </NavigationItem>
+      </li>
+      <li className="navigation-item" onClick={props.clicked}>
+        <NavigationItem link="/contact">
+          {context.dictionary.nav.contact}
+        </NavigationItem>
+      </li>
+      <li
+        className="navigation-item mobile-only"
+        onClick={props.clicked}
+      >
+        <NavigationItem link="/projects">
+          {context.dictionary.nav.projects}
+        </NavigationItem>
+      </li>
+      <li
+        className="navigation-item mobile-only"
+        onClick={props.clicked}
+      >
+        <NavigationItem link="/projects-premium">
+          &nbsp;&rarr;&nbsp;Premium
+        </NavigationItem>
+      </li>
+      <li
+        className="navigation-item mobile-only"
+        onClick={props.clicked}
+      >
+        <NavigationItem exact link="/realisations">
+          {context.dictionary.nav.realisations}
+        </NavigationItem>
+      </li>
+      <li
+        className="navigation-item mobile-only"
+        onClick={props.clicked}
+      >
+        <NavigationItem exact link="/realisations-premium">
+          &nbsp;&rarr;&nbsp;Premium
+        </NavigationItem>
+      </li>
+      <li className="navigation-item" onClick={props.clicked}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="navigation-item__link navigation-item__link--icon"
+          href="https://www.facebook.com/ProjektWPAW/"
+        >
+            <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default NavigationItems;
